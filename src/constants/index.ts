b4cf622a9ee88f2459e4fb8f99d4d1fde0ae45e1
gracebir <grace.devolop@gmail.com>
2023-08-31 13:17:53 +0200

    feat: define constants data

---
+++ src/constants/index.ts
@@ -0,0 +1,46 @@
+export const headerItems = [
+    {
+        id: 1,
+        title: "Company",
+        href: "#"
+    },
+    {
+        id: 2,
+        title: "People",
+        href: "#"
+    },
+    {
+        id: 3,
+        title: "Values",
+        href: "#"
+    },
+    {
+        id: 4,
+        title: "Product",
+        href: "#"
+    }
+]
+
+export const heroSection = {
+    title: 'Techno Sentience',
+    description: 'Our mission is to empower humanity\'s evolution through innovation, technology, and cosmic progress',
+    imageMob: "/images/homepage/hero-mobile.jpg",
+    imageDesk: "/images/homepage/hero-desk.jpg"
+}
+
+export const whoWeAre = {
+    header: "Who are we",
+    description: "Embracing techno-sentience, we're innovators weaving technology into human experience. Architects of ethical progress, harmonizing virtual and real, shaping a boundless future with purposeful connectivity",
+    image: "/images/homepage/who-we-are.jpg"
+}
+
+export const whatWevalue = {
+    header: "What we value",
+    description: "We appreciate technology's seamless integration into modern life, recognizing it as integral to life's essence. Our values reflect this harmony, where technology and life evolve hand in hand",
+    image: "/images/homepage/what-we-value.jpg"
+}
+
+export const vision = {
+    description: "Introducing Our Visionary Team: Pioneers of Omnihale Solutions for a Transformed Human Experience"
+}
+
