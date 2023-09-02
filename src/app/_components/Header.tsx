2854eaaa5d9149224a75e4a23e0cbf87436a4e84
gracebir <grace.devolop@gmail.com>
2023-09-02 15:24:21 +0200

    feat: adding open modal dialog variant

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,18 +1,27 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { Caesar_Dressing } from 'next/font/google'
-import { RxHamburgerMenu } from 'react-icons/rx'
 import { IoMenuOutline } from 'react-icons/io5'
+import { IoMdClose } from 'react-icons/io'
+import { AnimatePresence, motion, useCycle } from "framer-motion"
 import Link from 'next/link'
 import data from '@/constants/data.json'
+import HeaderModal from './HeaderModal'
 
 const caesar = Caesar_Dressing({ subsets: ['latin'], weight: '400' })
 
 const Header = () => {
+  const [openNav, toggleNav] = useCycle(false, true)
   return (
-    <header className='w-full py-4'>
+    <motion.header className='w-full py-4 relative'>
       <div className='container mx-auto flex justify-between items-center px-7 lg:px-0 lg:items-end'>
-        <IoMenuOutline className="block text-black cursor-pointer lg:hidden" size={30} />
+        <motion.button animate={openNav ? "open" : "closed"} className='z-10' onClick={() => toggleNav()}>
+          {openNav ?
+            <IoMdClose className="block text-black cursor-pointer lg:hidden" size={30} />
+            : <IoMenuOutline className="block text-black cursor-pointer lg:hidden" size={30} />
+          }
+        </motion.button>
         <div className='flex items-center gap-2'>
           <Image src="/logo.svg" alt='logo' className="w-[30px] h-[30px] lg:h-10 lg:w-10" width={40} height={40} />
           <h3 className={`${caesar.className} lg:text-2xl text-xl`}>Omnihale</h3>
@@ -25,7 +34,8 @@ const Header = () => {
         <Link key={data.headerItems[data.headerItems.length - 1].id} className='text-[1.125rem] text-text-color duration-300 hover:text-gray-700 hidden lg:block' href={data.headerItems[data.headerItems.length - 1].href}>{data.headerItems[data.headerItems.length - 1].title}</Link>
         <div className='block lg:hidden' />
       </div>
-    </header>
+      {openNav && (<AnimatePresence><HeaderModal toggle={toggleNav} /></AnimatePresence>)}
+    </motion.header>
   )
 }
 
