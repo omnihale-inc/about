3d7d9fe99969c8080f586a444f39fc3c7bcf15ea
gracebir <grace.devolop@gmail.com>
2023-09-03 07:36:24 +0200

    feat: adding variant

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -55,6 +55,60 @@ export const staggerContainer = (staggerChildren?: any, delayChildren?: any) => 
   },
 });
 
+export const navMenu = ({
+  initial: {
+    scaleY: 0,
+  },
+  animate: {
+    scaleY: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.12, 0, 0.39, 0],
+    },
+  },
+  exit: {
+    scaleY: 0,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+})
+
+export const staggerNavigation = ({
+  initial: {
+    transition: {
+      staggerChildren: 0.09,
+      staggerDirection: -1,
+    },
+  },
+  open: {
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.09,
+      staggerDirection: 1,
+    },
+  },
+})
+
+export const navLinkVariant = ({
+  initial: {
+    y: "30vh",
+    transition: {
+      duration: 0.5,
+      ease: [0.37, 0, 0.63, 1],
+    },
+  },
+  open: {
+    y: 0,
+    transition: {
+      ease: [0, 0.55, 0.45, 1],
+      duration: 0.7,
+    },
+  },
+})
+
 export const textVariant = (delay: number) => ({
   hidden: {
     y: 50,
