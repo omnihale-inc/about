1ac61a790407b21a46cc31ee5b07721f45fb2538
gracebir <grace.devolop@gmail.com>
2023-10-11 10:42:00 +0200

    making the with 100 percent

--- src/components/UserTeam.tsx
+++ src/components/UserTeam.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const UserTeam = ({ imgUrl, username, position }: { imgUrl: string, username: string, position: string }) => {
     return (
-        <div className='flex flex-col flex-1 w-[320px] col-span-1 bg-team-bg'>
+        <div className='flex flex-col flex-1 w-[320px] lg:w-full col-span-1 bg-team-bg'>
             <div className='w-full h-[25rem]'>
-                <Image className='w-full h-full object-contain' src={imgUrl} alt={username} width={320} height={400} />
+                <Image className='w-full h-full object-cover object-center' src={imgUrl} alt={username} width={320} height={400} />
             </div>
             <div className="py-6 flex flex-col items-center gap-2">
                 <h1 className='text-xl font-medium text-center '>{username}</h1>
